0064b8d397cb078d43a7807fbabe841433dc5de2
Ravi Kabra <b240744@skit.ac.in>
2026-06-23 17:37:03 +0530

    Increase free limit to 100 for testing

--- server/middleware/quota.js
+++ server/middleware/quota.js
@@ -10,7 +10,7 @@
 const logger = require('../utils/logger');
 const { getUserDailyUsage } = require('../services/firebase');
 
-const FREE_DAILY_LIMIT = 3;
+const FREE_DAILY_LIMIT = 100;
 
 // In-memory store: { ip: { date: 'YYYY-MM-DD', count: N } }
 const ipQuotaStore = new Map();
